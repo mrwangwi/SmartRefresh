086c55d929fa9e46d73bc0d09d34fe2ec1b79384
wangwei <wangwei@zjjxsoft.com>
2024-05-06 12:00:03 +0800

    ss

---
+++ app/src/main/java/com/jx/watchos/smartrefresh/MainActivity.java
@@ -0,0 +1,6 @@
+package com.jx.watchos.smartrefresh;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
---
+++ refresh-layout/src/main/java/com/scwang/smart/refresh/layout/SmartRefresh.java
@@ -0,0 +1,26 @@
+package com.scwang.smart.refresh.layout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class SmartRefresh extends SmartRefreshLayout {
+    public SmartRefresh(Context context) {
+        super(context);
+        init();
+    }
+
+    public SmartRefresh(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        setEnableLoadMore(false);
+        setEnableRefresh(false);
+        setEnableOverScrollDrag(true);
+        setRefreshHeader(new ClassicsHeader(getContext()));
+        setRefreshFooter(new ClassicsFooter(getContext()));
+        setReboundDuration(2000);
+        setEnableOverScrollBounce(true);
+    }
+}
